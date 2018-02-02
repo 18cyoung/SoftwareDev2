d5519f4947df95484f73748e1736756015a5dd2d
18cyoung <18cyoung@mlmc.vic.edu.au>
2018-02-02 11:57:17 +1100

    Did the Pythagoras Task and added trig

--- Maths/Pythagoras.py
+++ Maths/Pythagoras.py
@@ -1,8 +1,14 @@
 import math
 
-a = int(input("Please enter your 'A' value: "))
-b = int(input("Please enter your 'B' value: "))
+a = int(input("Please enter your 'a' value: "))
+b = int(input("Please enter your 'b' value: "))
 
 c = math.sqrt(a**2+b**2)
 
-print("Your 'C' Value is:",c)
+ang1 = math.acos(a/c)
+ang2 = math.atan(a/b)
+
+print("Your 'c' Value (Hypotenuse) is:",c)
+print("The interior angle is", round(ang1*(180/3.14159265359),1), "degrees")
+print("The interior angle is", round(ang2*(180/3.14159265359),1), "degrees")
+print("The interior angle is 90 degrees")
